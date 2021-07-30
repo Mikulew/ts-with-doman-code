2010c3c4c7ad3dc6e3e767a8647e07e3ea813cc3
Mikulew <mikulew@gmail.com>
2021-07-30 12:55:27 +0200

    mapToElementLength and enrichWithIndex comparision

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 "use strict";
-function selectionSort(arr) {
-    var _a;
-    for (var i = 0; i < arr.length; i++) {
-        var minIndex = findMinIndex(arr, i);
-        _a = [arr[minIndex], arr[i]], arr[i] = _a[0], arr[minIndex] = _a[1]; // swap
-    }
+function mapToElementLengthTypes(array) {
+    return array.map(function (e) { return e.length; });
 }
-function findMinIndex(arr, startIndex) {
-    var minValue = arr[startIndex];
-    var minIndex = startIndex;
-    for (var i = startIndex + 1; i < arr.length; i++) {
-        var currentValue = arr[i]; // i is next element
-        if (currentValue < minValue) {
-            minValue = currentValue;
-            minIndex = i;
-        }
-    }
-    return minIndex;
-}
-var arr = [4, 2, 6, 3, 1, 5];
-console.log('arr before:', arr);
-selectionSort(arr);
-console.log('arr after:', arr);
+var map1 = mapToElementLengthTypes(["this", "works!"]);
+console.log('map1:', map1);
+var map2 = mapToElementLengthTypes(["this", "works!", [3, 5, 7, 4]]);
+console.log('map2:', map2);
--- index.ts
+++ index.ts
@@ -1,25 +1,23 @@
-function selectionSort(arr: number[]) {
-    for (let i = 0; i < arr.length; i++) {
-        const minIndex = findMinIndex(arr, i);
-        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];  // swap
-    }
+function mapToElementLengthTypes(array: { length: number }[]): number[] {
+    return array.map(e => e.length);
 }
 
-function findMinIndex(arr: number[], startIndex: number) {
-    let minValue = arr[startIndex];
-    let minIndex = startIndex;
-    for (let i = startIndex + 1; i < arr.length; i++) {
-        const currentValue = arr[i]; // i is next element
-        if (currentValue < minValue) {
-            minValue = currentValue;
-            minIndex = i;
-        }
-    }
+const map1 = mapToElementLengthTypes(["this", "works!"]);
+console.log('map1:', map1);
+const map2 = mapToElementLengthTypes(["this", "works!", [3, 5, 7, 3]]);
+console.log('map2:', map2);
+const map3 = mapToElementLengthTypes(["this", "works!", [3, 5, 7, 3]], 5);
 
-    return minIndex;
+function enrichWithIndex(array: any[]): { index: number, value: any }[] {
+    return array.map((e, index) => ({ value: e, index }));
 }
 
-const arr = [4, 2, 6, 3, 1, 5];
-console.log('arr before:', arr);
-selectionSort(arr);
-console.log('arr after:', arr);
+const result = enrichWithIndex(["hey", "there", "whats up"]);
+console.log(result[0].value.notExistingMethod());
+
+function enrichWithIndexTyped<T>(array: T[]): { index: number, value: T }[] {
+    return array.map((e, index) => ({ value: e, index }));
+}
+
+const result2 = enrichWithIndexTyped(["hey", "there", "whats up"]);
+console.log(result2[0].value.notExistingMethod());
