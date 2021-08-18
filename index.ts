7c882ddeb82cba2be6e9cca2446bef710a87a69f
Mikulew <mikulew@gmail.com>
2021-08-18 16:09:24 +0200

    Klasy cz.1 #09

--- index.ts
+++ index.ts
@@ -1,41 +1,23 @@
-type UserId = number | string | null | undefined;
-type NonNullableUserId = NonNullable<UserId>;
+class Person {
+    #pesel = 11122233344;
 
-const userId: NonNullableUserId = null; // Type 'null' is not assignable to type 'NonNullableUserId'.
-
-function someFunction(age: number, id: number, firstName: string, lastName: string) {
-    console.log(age, id, firstName, lastName);
-}
-
-type SomeFunction = Parameters<typeof someFunction>; // [age: number, id: number, firstName: string, lastName: string]
-
-const parametersForOurFunction: SomeFunction = [4, 15, 'Aleksander', 'Krawiec'];
-
-someFunction(...parametersForOurFunction);
-
-class User {
     constructor(
         public age: number,
-        public id: number,
-        public firstName: string,
-        public lastName: string,
+        private firstName: string, 
+        protected lastName: string
     ) {}
+
+    changeFirstName(newName: string) {
+        this.firstName = newName;
+    }
+
+    greetings() {
+        console.log(`Hello! My name is ${this.firstName} ${this.lastName}`);
+    }
+
+    showPesel() {
+        console.log(this.#pesel);
+    }
 }
 
-type UserClassParameters = ConstructorParameters<typeof User>; // [age: number, id: number, firstName: string, lastName: string]
-
-const newUserParams: UserClassParameters = [4, 15, 'Dominik', 'Szewc'];
-
-new User(...newUserParams);
-
-type Person = {
-    age: number,
-    firstName: string,
-    lastName: string,
-}
-
-function gerFirstName(person: Person) {
-    return person.age;
-}
-
-const age: ReturnType<typeof gerFirstName> = 31;
+const user = new Person(31, "Mateusz", "Domański");
